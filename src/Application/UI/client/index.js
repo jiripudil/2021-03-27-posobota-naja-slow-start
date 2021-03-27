497cc2cc1f37d312d16fffcf2b282953fc2acf12
Jiří Pudil <me@jiripudil.cz>
2021-03-27 11:14:14 +0100

    import and initialize naja
    By default, naja attaches itself to all links and forms marked with class "ajax".

--- src/Application/UI/client/index.js
+++ src/Application/UI/client/index.js
@@ -1,7 +1,10 @@
+import naja from 'naja';
 import 'nette-forms';
 import './index.css';
 
 document.addEventListener('DOMContentLoaded', () => {
+	naja.initialize();
+
 	const categoryFilter = document.querySelector('.categoryFilter select');
 	if (categoryFilter !== null) {
 		categoryFilter.addEventListener('change', (event) => {
