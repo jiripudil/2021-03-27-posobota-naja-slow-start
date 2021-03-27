65a5b55915e92c7cc31c3c8a66ee8daa5818f7e1
Jiří Pudil <me@jiripudil.cz>
2021-03-27 11:56:21 +0100

    Localize the loading spinner
    However, we might want to show the spinner over a smaller component. Let's make the extension more generic - on 'interaction', find the closest ancestor that matches a given selector, and use 'options' to pass that information between various event listeners.

--- src/Application/UI/client/index.js
+++ src/Application/UI/client/index.js
@@ -17,16 +17,23 @@ class CategoryFilterExtension {
 }
 
 class SpinnerExtension {
+	constructor(selector) {
+		this.selector = selector;
+	}
+
 	initialize(naja) {
-		const mainContent = document.querySelector('.mainContent');
-		naja.addEventListener('start', () => mainContent.classList.add('spinner'));
-		naja.addEventListener('complete', () => mainContent.classList.remove('spinner'));
+		naja.uiHandler.addEventListener('interaction', (event) => {
+			event.detail.options.spinnerTarget = event.detail.element.closest(this.selector) ?? document.querySelector('.mainContent');
+		});
+
+		naja.addEventListener('start', (event) => event.detail.options.spinnerTarget?.classList.add('spinner'));
+		naja.addEventListener('complete', (event) => event.detail.options.spinnerTarget?.classList.remove('spinner'));
 	}
 }
 
 document.addEventListener('DOMContentLoaded', () => {
 	naja.uiHandler.selector = ':not([data-naja-off])';
 	naja.registerExtension(new CategoryFilterExtension());
-	naja.registerExtension(new SpinnerExtension());
+	naja.registerExtension(new SpinnerExtension('[data-spinner]'));
 	naja.initialize();
 });
