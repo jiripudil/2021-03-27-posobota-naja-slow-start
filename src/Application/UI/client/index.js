a5e8d4e53786dde77b84a36f07b32b7d9fb42883
Jiří Pudil <me@jiripudil.cz>
2021-03-27 11:47:19 +0100

    Wrap the category filter in an extension
    We don't want to pollute the index.js file in the long run. Naja provides a mechanism of extensions to wrap together single coherent units of behaviour like this. The only requirement is that an extension implements the initialize(naja) method.

--- src/Application/UI/client/index.js
+++ src/Application/UI/client/index.js
@@ -2,17 +2,22 @@ import naja from 'naja';
 import 'nette-forms';
 import './index.css';
 
+class CategoryFilterExtension {
+	initialize(naja) {
+		const enableCategoryFilter = (element) => {
+			const categoryFilter = element.querySelector('.categoryFilter select');
+			categoryFilter?.addEventListener('change', (event) => {
+				naja.uiHandler.submitForm(event.target.form);
+			});
+		}
+
+		enableCategoryFilter(document);
+		naja.snippetHandler.addEventListener('afterUpdate', (event) => enableCategoryFilter(event.detail.snippet));
+	}
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 	naja.uiHandler.selector = ':not([data-naja-off])';
+	naja.registerExtension(new CategoryFilterExtension());
 	naja.initialize();
-
-	const enableCategoryFilter = (element) => {
-		const categoryFilter = element.querySelector('.categoryFilter select');
-		categoryFilter?.addEventListener('change', (event) => {
-			naja.uiHandler.submitForm(event.target.form);
-		});
-	}
-
-	enableCategoryFilter(document);
-	naja.snippetHandler.addEventListener('afterUpdate', (event) => enableCategoryFilter(event.detail.snippet));
 });
